f2512cd8150c44e2ef1b237fc8758f5ba6f82352
黄俊铭 <huangjunming@huangjunmingdeMacBook-Pro.local>
2019-12-12 01:38:59 +0800

    feat: 集成 redux 同构

--- client/index.js
+++ client/index.js
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from '../src/App'
+import store from '../src/store/store'
+import { Provider } from 'react-redux'
 
 // 注水, 客户端入口 
-const Page = (< BrowserRouter >
-  {App}
-</BrowserRouter >)
+const Page = (<Provider store={store}>
+  < BrowserRouter >
+    {App}
+  </BrowserRouter >
+</Provider>)
 ReactDom.hydrate(Page, document.getElementById('root'))
