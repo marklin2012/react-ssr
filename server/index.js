1e67add8eaac4e2fb9578f4a3766ba506bc7dfb5
黄俊铭 <huangjunming@huangjunmingdeMacBook-Pro.local>
2019-12-12 00:56:51 +0800

    feat: 服务端及页面支持多页面 ssr

--- server/index.js
+++ server/index.js
@@ -3,13 +3,17 @@ import React from 'react'
 import { renderToString } from 'react-dom/server'
 import express from 'express'
 import App from '../src/App'
+import { StaticRouter } from 'react-router-dom'
 
 const app = express()
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
+
   // 把react组件，解析成html
-  const content = renderToString(App)
+  const content = renderToString(
+    <StaticRouter location={req.url}>{App}</StaticRouter>
+  )
   res.send(`
   <html>
     <head>
