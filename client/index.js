1dce4e75f67bcbd8919f46b53b5b1e1e1d1d9319
黄俊铭 <huangjunming@huangjunmingdeMacBook-Pro.local>
2019-12-15 16:22:52 +0800

    fix: 页面组件报错

--- client/index.js
+++ client/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import routes from '../src/App'
 import { getClientStore } from '../src/store/store'
 import { Provider } from 'react-redux'
